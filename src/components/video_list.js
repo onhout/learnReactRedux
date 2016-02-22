c2057745d3e16937835daf0f5a13e8bf577098b2
Peter Liu <onhout@gmail.com>
2016-02-22 11:26:04 -0800

    react beginner

---
+++ src/components/video_list.js
@@ -0,0 +1,3 @@
+/**
+ * Created by pl on 2/22/16.
+ */
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 /**
  * Created by pl on 2/22/16.
  */
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+    return <div>Hi!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
