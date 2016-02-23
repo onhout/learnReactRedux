344ed9b6b077f4f983f49e16395429a1453c8463
Peter Liu <onhout@gmail.com>
2016-02-23 09:56:20 -0800

    refactor the component to become a class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,3 +1,13 @@
 /**
  * Created by pl on 2/22/16.
  */
+
+import React from 'react';
+
+class SearchBar extends React.Component{
+	render(){
+		return <input />;
+	}
+}
+
+export default SearchBar;
