a11da3b7b8c76b07375b8a417a2af75d78f4f3e2
Peter Liu <onhout@gmail.com>
2016-02-25 11:59:59 -0800

    videos now pops upp using react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 /**
  * Created by pl on 2/22/16.
  */
-import React from 'react'
+import React , {Component} from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+const API_KEY = 'AIzaSyCbHb455PAsw5mUySCnxtK_KXretllnWFQ'; 
 
-const App = () => {
-    return <div><SearchBar /></div>;
+
+
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state={ videos:[] };
+
+		YTSearch({key: API_KEY, term: 'cats'}, (videos) => {
+			this.setState({videos});
+			//this.setState({videos:videos});
+			//syntactic sugar
+		});
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+    	);
+	}
+    
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
